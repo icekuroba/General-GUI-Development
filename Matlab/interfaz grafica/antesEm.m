dee274c7563f388e8942c9724ebb0b017c996361
IceKuroba <130119725+icekuroba@users.noreply.github.com>
2024-07-03 12:34:20 -0600

    Create antesEm.m

---
+++ Matlab/interfaz grafica/antesEm.m
@@ -0,0 +1,4 @@
+restoredefaultpath;
+%sustituir por la ruta donde se guardo la toolbox fieldtrip
+addpath 'C:\Users\Dl\AppData\Roaming\MathWorks\MATLAB Add-Ons\Collections\Peak finding and measurement, 2019\fieldtrip-20240110';
+ft_defaults;
